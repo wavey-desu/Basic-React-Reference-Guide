1eaecfb40768e30c8ae64b338cc28ecdbb190d1f
ALEJANDRO BARRERA FLORES <wavey.dev0@gmail.com>
2022-08-01 13:51:06 -0500

    create-reactapp | UseStates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import Title from './components/Title'
 
 function App() {
+  // const [name, setName] = useState('what my name is ')
+  const [showEvents, setShowEvents] = useState(true)
+  const [events, setEvents] = useState([
+    {title: 'what', id:1 },
+    {title: 'who ', id:2},
+    {title: 'chk chk slim shady', id:3}
+  ])
+
+  const handleClick = (id)=>{
+    // setName('chk chk slim shady')
+    setEvents((prevEvents)=> { // previous state value guarantee to stay up to date
+      return prevEvents.filter((e)=>{  //whatever u state update, depends on the previous state | always use callback to acces previous state
+        return id !== e.id   //return only if !=
+      })
+    })
+    console.log(id);
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Title />
+      {/* <h1>my name is {name}</h1> */}
+      {/* <button onClick={handleClick}>who</button> */}
+
+      {showEvents && events.map( (e,index)=> ( //only show events if showEvents true
+        <div key={e.id}>
+          <h2>My name is {e.title}</h2>
+          <button onClick={()=>handleClick(e.id)}>ok</button>
+        </div>
+      ) )}
+
+      <div>
+        {/* Show btn when showEvents True */}
+        {showEvents && ( <button onClick={()=>setShowEvents(false)}>hide</button>)} 
+        {/* Show btn when showEvents False */}
+        {!showEvents && (<button onClick={()=>setShowEvents(true)}>show</button>)}
+      </div>
     </div>
+    
   );
 }
 
