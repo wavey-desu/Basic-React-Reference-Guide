234bfcc98afbf60a1b7b1b5511010b32f4be24e6
ALEJANDRO BARRERA FLORES <wavey.dev0@gmail.com>
2022-08-01 13:48:02 -0500

    create-reactapp | UseStates

---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+export default function Title() {
+    return (
+        <div>
+            <h1 className="title">Im the real shady</h1>
+            <br />
+            <h2 className="subtitle">yes im the real shady</h2>
+        </div>
+    )
+}
